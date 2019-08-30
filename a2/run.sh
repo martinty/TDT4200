db8cd97c7a5984ebc94d068fe9b14c27f9fef7ed
Martin Tysseland <martinty@stud.ntnu.no>
2019-08-30 12:49:14 +0200

    Update a2

---
+++ a2/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd)"
+cd $SCRIPTPATH
+
+if [[ $1 == "new" ]]; then
+    git clean -dfx program/
+    git clean -dfx build/
+elif [[ $1 == "clean" ]]; then
+    cd build
+    make clean
+    exit 0
+fi
+
+mkdir -p build
+mkdir -p program
+cd build
+
+if [[ $1 != "run" ]] && [[ $1 != "gdb" ]]; then
+    echo "------------- Running cmake -------------"
+    cmake ..
+fi
+
+if [[ $OSTYPE =~ "linux" ]]; then
+    echo "------------- Running make --------------"
+    make -j4
+    cd ../program
+    if [[ $1 == "gdb" ]]; then
+        echo "------------- Running program with gdb --"
+        gdb ./bitmap
+    elif [[ $1 == "run" ]]; then
+        echo "------------- Running program -----------"
+        ./bitmap
+    fi
+else
+    echo "$OSTYPE is not supported for make and run!"
+fi
