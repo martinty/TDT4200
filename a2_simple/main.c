e6887789f3f95d0db50a3dc9278b106d7fd62151
Martin Tysseland <martinty@stud.ntnu.no>
2019-09-03 17:42:02 +0200

    Add MPI_Send and MPI_Recv

--- a2_simple/main.c
+++ a2_simple/main.c
@@ -19,34 +19,53 @@ int main(int argc, char** argv) {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    // Make Pixel datatype
+    MPI_Datatype pixel_dt;
+    MPI_Type_contiguous(3, MPI_UNSIGNED_CHAR, &pixel_dt);
+    MPI_Type_commit(&pixel_dt);
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
-           
+    // Set size of array sent to each process
+    int yScale = YSIZE / world_size;
 
     if(world_rank == 0){
         Pixel **image = calloc(YSIZE, sizeof(Pixel *));
         for(int row = 0; row < YSIZE; row++){
             image[row] = calloc(XSIZE, sizeof(Pixel));
         }
-	    
+
         readbmp("before.bmp", image);
 
-	    // Alter the image here
-        invertColor(image, XSIZE, YSIZE);
+        int rest = YSIZE % world_size;
+        for(int i = 1; i < world_size; i++){
+            MPI_Send(&image[yScale*i+rest][0], XSIZE*yScale, pixel_dt, i, 0, MPI_COMM_WORLD);
+        }
+        //invertColor(image, XSIZE, yScale + rest);
+        for(int i = 1; i < world_size; i++){
+            MPI_Recv(&image[yScale*i+rest][0], XSIZE*yScale, pixel_dt, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
 
-    	savebmp("after.bmp", image, XSIZE, YSIZE);
-	    
+	    savebmp("after.bmp", image, XSIZE, YSIZE);
+
         for(int row = 0; row < YSIZE; row++){
             free(image[row]);
         }
         free(image);
-    };
+    }
+    else{
+        Pixel **data = calloc(yScale, sizeof(Pixel *));
+        for(int row = 0; row < yScale; row++){
+            data[row] = calloc(XSIZE, sizeof(Pixel));
+        }
+
+        MPI_Recv(&data[0][0], XSIZE*yScale, pixel_dt, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        invertColor(data, XSIZE, yScale);
+        MPI_Send(&data[0][0], XSIZE*yScale, pixel_dt, 0, 0, MPI_COMM_WORLD);
+
+        for(int row = 0; row < yScale; row++){
+            free(data[row]);
+        }
+        free(data);
+    }
     
 
     // Finalize the MPI environment.
