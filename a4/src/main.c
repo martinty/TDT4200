3346279ec9da84de0f4cad36679952a3a8fbd2b0
Martin Tysseland <martinty@stud.ntnu.no>
2019-10-03 21:49:12 +0200

    Optimization

--- a4/src/main.c
+++ a4/src/main.c
@@ -15,14 +15,13 @@
 // first step MUST be 0.0
 // last step MUST be 1.0
 static const struct colourGradientStep colourGradient[colourGradientSteps] = {
-	{ .step = 0.0 , { .r = 0  , .g = 0  , .b = 0   }},
-	{ .step = 0.03, { .r = 0  , .g = 7  , .b = 100 }},
-	{ .step = 0.16, { .r = 32 , .g = 107, .b = 203 }},
-	{ .step = 0.42, { .r = 237, .g = 255, .b = 255 }},
-	{ .step = 0.64, { .r = 255, .g = 170, .b = 0   }},
-	{ .step = 0.86, { .r = 0  , .g = 2  , .b = 0   }},
-	{ .step = 1.0,  { .r = 0  , .g = 0  , .b = 0   }}
-};
+	{.step = 0.0, {.r = 0, .g = 0, .b = 0}},
+	{.step = 0.03, {.r = 0, .g = 7, .b = 100}},
+	{.step = 0.16, {.r = 32, .g = 107, .b = 203}},
+	{.step = 0.42, {.r = 237, .g = 255, .b = 255}},
+	{.step = 0.64, {.r = 255, .g = 170, .b = 0}},
+	{.step = 0.86, {.r = 0, .g = 2, .b = 0}},
+	{.step = 1.0, {.r = 0, .g = 0, .b = 0}}};
 
 // Gets initialized after parameters are parsed
 static pixel *colourMap = NULL;
@@ -31,92 +30,60 @@ static unsigned int colourMapSize = 0;
 static unsigned int res = 2048;
 static unsigned int maxDwell = 512;
 
-
-pixel getDwellColour(unsigned int const y, unsigned int const x, unsigned long long const dwell) {
+pixel getDwellColour(unsigned int const y, unsigned int const x, unsigned long long const dwell)
+{
 	static const double log2 = 0.693147180559945309417232121458176568075500134360255254120;
 	double complex z = x + y * I;
-	unsigned long long index = dwell + 1 - log(log(cabs(z)/log2));
+	unsigned long long index = dwell + 1 - log(log(cabs(z) / log2));
 	return colourMap[index % colourMapSize];
 }
 
-
-double complex getInitialValue(double complex const cmin, double complex const cmax, unsigned int const y, unsigned int const x) {
-	double real = ((double) x / res) * creal(cmax - cmin) + creal(cmin);
-	double imag = ((double) y / res) * cimag(cmax - cmin) + cimag(cmin);
+double complex getInitialValue(double complex const cmin, double complex const cmax, unsigned int const y, unsigned int const x)
+{
+	double real = ((double)x / res) * creal(cmax - cmin) + creal(cmin);
+	double imag = ((double)y / res) * cimag(cmax - cmin) + cimag(cmin);
 	double complex ret = real + imag * I;
 	return ret;
 }
 
-double complex computeNextValue(double complex const z, double complex const init) {
-	return (z * z) + init;
-}
-
-bool isPartOfMandelbrot(double complex const z, double const factor) {
-	return cabs(z) < (factor * factor);
-}
-
-unsigned long long pixelDwell(double complex const cmin,
-						double complex const cmax,
-						unsigned int const y,
-						unsigned int const x)
+unsigned long long pixelDwell(double complex const cmin, double complex const cmax, unsigned int const y, unsigned int const x)
 {
-	double complex z = getInitialValue(cmin, cmax, y, x);
-	unsigned int const dwellInc = 1;
+	double complex const init = getInitialValue(cmin, cmax, y, x);
+	double complex z = init;
 	unsigned long long dwell = 0;
 
-	// Exit condition: dwell is maxDwell or |z| >= 4
-	while(dwell < maxDwell && isPartOfMandelbrot(z, 2.0)) {
-		// z = z² + initValue
-		z = computeNextValue(z, getInitialValue(cmin, cmax, y, x));
-		dwell += dwellInc;
+	while (dwell < maxDwell && cabs(z) < 4.0)
+	{
+		z = z * z + init;
+		dwell++;
 	}
 
 	return dwell;
 }
 
-void computeDwellBuffer(unsigned long long **buffer, double complex cmin, double complex cmax) {
-	for (unsigned int x = 0; x < res; x++) {
-		for (unsigned int y = 0; y < res; y++) {
-			buffer[y][x] = pixelDwell(cmin, cmax, y, x);
+void computeAndMapDwell(bmpImage *image, double complex cmin, double complex cmax)
+{
+	for (unsigned int y = 0; y < res; y++)
+	{
+		for (unsigned int x = 0; x < res; x++)
+		{
+			image->data[y][x] = getDwellColour(y, x, pixelDwell(cmin, cmax, y, x));
 		}
 	}
 }
 
-void mapDwellBuffer(bmpImage *image, unsigned long long **buffer) {
-	for (unsigned int x = 0; x < res; x++) {
-		for (unsigned int y = 0; y < res; y++) {
-			pixel *colour = malloc(sizeof(pixel));
-			*colour = getDwellColour(y, x, buffer[y][x]);
-			image->data[y][x].r = colour->r;
-			image->data[y][x].g = colour->g;
-			image->data[y][x].b = colour->b;
-			free(colour);
-		}
-	}
-}
-
-void computeAndMapDwellBuffer(bmpImage *image, unsigned long long **buffer, double complex cmin, double complex cmax) {
-	for (unsigned int y = 0; y < res; y++) {
-		for (unsigned int x = 0; x < res; y++) {
-			buffer[y][x] = pixelDwell(cmin, cmax, y, x);
-			pixel *colour = malloc(sizeof(pixel));
-			*colour = getDwellColour(y, x, buffer[y][x]);
-			image->data[y][x].r = colour->r;
-			image->data[y][x].g = colour->g;
-			image->data[y][x].b = colour->b;
-			free(colour);
-        }
-    }
-}
-
-
-void help(char const *exec, char const opt, char const *optarg) {
+void help(char const *exec, char const opt, char const *optarg)
+{
 	FILE *out = stdout;
-	if (opt != 0) {
+	if (opt != 0)
+	{
 		out = stderr;
-		if (optarg) {
+		if (optarg)
+		{
 			fprintf(out, "Invalid parameter - %c %s\n", opt, optarg);
-		} else {
+		}
+		else
+		{
 			fprintf(out, "Invalid parameter - %c\n", opt);
 		}
 	}
@@ -130,57 +97,60 @@ void help(char const *exec, char const opt, char const *optarg) {
 	fprintf(out, "%s [options]  <output-bmp>\n", exec);
 }
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
 	int ret = 1;
 	/* Standard Values */
-	char *output = NULL; //output image filepath
-	bmpImage *image = NULL; //output image
-	unsigned long long **dwellBuffer = NULL;
-	double x = 0.5, y = 0.5; // coordinates
-	double scale = 1; // scaling factor
+	char *output = NULL;			   //output image filepath
+	bmpImage *image = NULL;			   //output image
+	double x = 0.5, y = 0.5;		   // coordinates
+	double scale = 1;				   // scaling factor
 	unsigned int colourIterations = 1; //how many times the colour gradient is repeated
-	bool quiet = false; //output something or not
+	bool quiet = false;				   //output something or not
 
 	/* Parameter parsing... */
 	{
 		char c;
-		while((c = getopt(argc,argv,"x:y:s:r:i:c:qh"))!=-1) {
-			switch(c) {
-				case 'x':
-					x = clampDouble(atof(optarg),0.0,1.0);
-					break;
-				case 'y':
-					y = clampDouble(atof(optarg),0.0,1.0);
-					break;
-				case 's':
-					scale = clampDouble(atof(optarg),0.0,1.0);
-					if (scale == 0) scale = 1;
-					break;
-				case 'r':
-					res = atoi(optarg);
-					break;
-				case 'i':
-					maxDwell = atoi(optarg);
-					break;
-				case 'c':
-					colourIterations = atoi(optarg);
-					break;
-				case 'q':
-					quiet = true;
-					break;
-				case 'h':
-					help(argv[0],0, NULL);
-					goto exit_graceful;
-					break;
-				default:
-					abort();
+		while ((c = getopt(argc, argv, "x:y:s:r:i:c:qh")) != -1)
+		{
+			switch (c)
+			{
+			case 'x':
+				x = clampDouble(atof(optarg), 0.0, 1.0);
+				break;
+			case 'y':
+				y = clampDouble(atof(optarg), 0.0, 1.0);
+				break;
+			case 's':
+				scale = clampDouble(atof(optarg), 0.0, 1.0);
+				if (scale == 0)
+					scale = 1;
+				break;
+			case 'r':
+				res = atoi(optarg);
+				break;
+			case 'i':
+				maxDwell = atoi(optarg);
+				break;
+			case 'c':
+				colourIterations = atoi(optarg);
+				break;
+			case 'q':
+				quiet = true;
+				break;
+			case 'h':
+				help(argv[0], 0, NULL);
+				goto exit_graceful;
+				break;
+			default:
+				abort();
 			}
 		}
 	}
 
-	if (argc <= (optind)) {
-		help(argv[0],' ',"Not enough arugments");
+	if (argc <= (optind))
+	{
+		help(argv[0], ' ', "Not enough arugments");
 		goto error_exit;
 	}
 
@@ -197,7 +167,6 @@ int main( int argc, char *argv[] )
 	initColourMap(colourMap, colourMapSize, colourGradient);
 
 	/* Putting some magic numbers in place...  */
-
 	double const xmin = -3.5 + (2 * 2 * x);
 	double const xmax = -1.5 + (2 * 2 * x);
 	double const ymin = -3.0 + (2 * 2 * y);
@@ -210,51 +179,29 @@ int main( int argc, char *argv[] )
 	double complex const cmax = (xmax - (0.5 * (1 - scale) * xlen)) + (ymax - (0.5 * (1 - scale) * ylen)) * I;
 
 	/* Output useful informations... */
-	if (!quiet) {
-		printf("Center:      [%f,%f]\n",x,y);
-		printf("Zoom:        %llu%%\n", (unsigned long long) (1/scale) * 100);
+	if (!quiet)
+	{
+		printf("Center:      [%f,%f]\n", x, y);
+		printf("Zoom:        %llu%%\n", (unsigned long long)(1 / scale) * 100);
 		printf("Iterations:  %u\n", maxDwell);
 		printf("Window:      Re[%f,%f], Im[%f,%f]\n", creal(cmin), creal(cmax), cimag(cmin), cimag(cmax));
 		printf("Output:      %s\n", output);
 	}
 
-
 	//Allocate bmp image
 	image = newBmpImage(res, res);
-	if (image == NULL) {
+	if (image == NULL)
+	{
 		fprintf(stderr, "ERROR: could not allocate bmp image space!\n");
 		goto error_exit;
 	}
 
-	//Allocate the Dwell buffer, 2 dimensional array
-	dwellBuffer = malloc(res * sizeof(unsigned long long *));
-	if (dwellBuffer == NULL) {
-		fprintf(stderr, "ERROR: could not allocate dwell buffer\n");
-		goto error_exit;
-	}
-	for (unsigned int y = 0; y < res; y++) {
-		dwellBuffer[y] = malloc(res * sizeof(unsigned long long));
-		if (dwellBuffer[y] == NULL) {
-			fprintf(stderr, "ERROR: could not allocate dwell buffer\n");
-			for (unsigned int j= 0; j < y; j++) {
-				free(dwellBuffer[j]);
-			}
-			goto error_exit;
-
-		}
-	}
-
-	//Compute the dwell buffer
-	//computeDwellBuffer(dwellBuffer, cmin, cmax);
-
-	//Map the dwell buffer to the bmpImage with fancy colors
-	//mapDwellBuffer(image, dwellBuffer);
-
-    // Compute the dwell buffer and map it to the bmpImage with fancy colors
-    computeAndMapDwellBuffer(image, dwellBuffer, cmin, cmax);
+	// Compute the dwell and map it to the bmpImage with fancy colors
+	computeAndMapDwell(image, cmin, cmax);
 
 	// Save the Image
-	if(saveBmpImage(image, output)) {
+	if (saveBmpImage(image, output))
+	{
 		fprintf(stderr, "ERROR: could not save image to %s\n", output);
 		goto error_exit;
 	}
@@ -263,17 +210,9 @@ int main( int argc, char *argv[] )
 error_exit:
 	ret = 1;
 exit_graceful:
-	if(image)
+	if (image)
 		freeBmpImage(image);
-	if(colourMap)
+	if (colourMap)
 		free(colourMap);
-	if(dwellBuffer) {
-		for (unsigned int i = 0; i < res; i++) {
-			free(dwellBuffer[i]);
-		}
-		free(dwellBuffer);
-	}
-
 	return 0;
 }
-
