adab71a6e9c4a6dd3e29c63a52a598fc1d7bfee1
Martin Tysseland <martinty@clab08.idi.ntnu.no>
2019-10-31 11:32:19 +0100

    Comment out unused filters

--- a7/main.cu
+++ a7/main.cu
@@ -26,6 +26,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
 // If you apply another filter, remember not only to exchange
 // the filter but also the filterFactor and the correct dimension.
 
+/*
 int const sobelYFilter[] = {-1, -2, -1,
                             0, 0, 0,
                             1, 2, 1};
@@ -35,6 +36,7 @@ int const sobelXFilter[] = {-1, -0, -1,
                             -2, 0, -2,
                             -1, 0, -1, 0};
 float const sobelXFilterFactor = (float)1.0;
+*/
 
 int const laplacian1Filter[] = {-1, -4, -1,
                                 -4, 20, -4,
@@ -42,6 +44,7 @@ int const laplacian1Filter[] = {-1, -4, -1,
 
 float const laplacian1FilterFactor = (float)1.0;
 
+/*
 int const laplacian2Filter[] = {0, 1, 0,
                                 1, -4, 1,
                                 0, 1, 0};
@@ -59,6 +62,7 @@ int const gaussianFilter[] = {1, 4, 6, 4, 1,
                               4, 16, 24, 16, 4,
                               1, 4, 6, 4, 1};
 float const gaussianFilterFactor = (float)1.0 / 256.0;
+*/
 
 // Apply convolutional filter on image data
 void applyFilter(unsigned char **out, unsigned char **in, unsigned int width, unsigned int height, int *filter, unsigned int filterDim, float filterFactor)
